c89ac5523b58f090560b349e8daa3af1b5ad98ec
yaelren <yaelre@wix.com>
2025-12-18 11:06:43 +0200

    Add tool.js for tool: 3d-pillar-chart

---
+++ public/tools/3d-pillar-chart/tool.js
@@ -0,0 +1,8 @@
+
+    {
+        "imports": {
+            "three": "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js",
+            "three/addons/": "https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/"
+        }
+    }
+    
